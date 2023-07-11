e0036d676dc7010e3e0e154f978bd0c14a9b1038
marcosrebelo97 <mgr@icomp.ufam.edu.br>
2023-07-11 16:31:14 -0400

    implementação CRUD

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -1,37 +1,60 @@
 package com.algaworks.algalog.api.controller;
 
 import com.algaworks.algalog.domain.model.Cliente;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.algaworks.algalog.domain.repository.ClienteRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+@AllArgsConstructor
 @RestController
+@RequestMapping(value = "/clientes")
 public class ClienteController {
-    @GetMapping(value = "/clientes")
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @GetMapping
     public List<Cliente> listar() {
-        Cliente cliente1 = new Cliente();
-        cliente1.setId(1L);
-        cliente1.setNome("Marcos");
-        cliente1.setTelefone("99987841");
-        cliente1.setNome("Marcosasbdhasbdhasd");
-        cliente1.setTelefone("23123123213");
-        cliente1.setEmail("marcosrebelo9@gmail.com");
+        return clienteRepository.findAll();
+    }
 
-        Cliente cliente2 = new Cliente();
-        cliente2.setId(2L);
-        cliente2.setNome("Rita");
-        cliente2.setTelefone("517841471");
-        cliente2.setEmail("rita@gmail.com");
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Cliente> buscar(@PathVariable Long id) {
+        return clienteRepository.findById(id)
+                .map(cliente -> ResponseEntity.ok(cliente))
+                .orElse(ResponseEntity.notFound().build());
+    }
 
-        Cliente cliente3 = new Cliente();
-        cliente3.setId(3L);
-        cliente3.setNome("dfsdfsdfsfsfsfsfsdfsdf");
-        cliente3.setTelefone("4841");
-        cliente3.setEmail("knasjdnasjdasn@gmail.com");
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente adicionar(@RequestBody Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
 
-        return Arrays.asList(cliente1, cliente2, cliente3);
+    @PutMapping(value = ("/{id}"))
+    public ResponseEntity<Cliente> atualizar(@PathVariable Long id, @RequestBody Cliente cliente) {
+        if (!clienteRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        cliente.setId(id);
+        cliente = clienteRepository.save(cliente);
+        return ResponseEntity.ok(cliente);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> excluir(Long id) {
+        if (!clienteRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        clienteRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/algaworks/algalog/domain/model/Cliente.java
+++ src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -1,11 +1,19 @@
 package com.algaworks.algalog.domain.model;
 
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class Cliente {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
