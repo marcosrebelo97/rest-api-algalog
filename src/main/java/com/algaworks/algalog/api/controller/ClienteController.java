f18fc86f295dae87249dfb9ed2c60ec5e8d07db4
marcosrebelo97 <mgr@icomp.ufam.edu.br>
2023-07-11 15:07:34 -0400

    migration ok

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -2,10 +2,36 @@ package com.algaworks.algalog.api.controller;
 
 import com.algaworks.algalog.domain.model.Cliente;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class ClienteController {
+    @GetMapping(value = "/clientes")
+    public List<Cliente> listar() {
+        Cliente cliente1 = new Cliente();
+        cliente1.setId(1L);
+        cliente1.setNome("Marcos");
+        cliente1.setTelefone("99987841");
+        cliente1.setNome("Marcosasbdhasbdhasd");
+        cliente1.setTelefone("23123123213");
+        cliente1.setEmail("marcosrebelo9@gmail.com");
 
+        Cliente cliente2 = new Cliente();
+        cliente2.setId(2L);
+        cliente2.setNome("Rita");
+        cliente2.setTelefone("517841471");
+        cliente2.setEmail("rita@gmail.com");
 
+        Cliente cliente3 = new Cliente();
+        cliente3.setId(3L);
+        cliente3.setNome("dfsdfsdfsfsfsfsfsdfsdf");
+        cliente3.setTelefone("4841");
+        cliente3.setEmail("knasjdnasjdasn@gmail.com");
+
+        return Arrays.asList(cliente1, cliente2, cliente3);
+    }
 }
---
+++ src/main/resources/db/migration/V1__criar-tabela.sql
@@ -0,0 +1,7 @@
+CREATE TABLE cliente (
+	id          SERIAL NOT NULL,
+	nome        varchar (50) NOT NULL,
+	email       varchar (50) NOT NULL,
+	telefone    varchar (20) NOT NULL,
+	PRIMARY KEY (id)
+	);
