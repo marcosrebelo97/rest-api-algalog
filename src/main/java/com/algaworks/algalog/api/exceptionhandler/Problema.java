a58507a9e172d2741cac7d4aaaf137bfa13444e2
marcosrebelo97 <mgr@icomp.ufam.edu.br>
2023-07-12 16:17:27 -0400

    especificando erro de excecao

--- src/main/java/com/algaworks/algalog/api/exceptionhandler/Problema.java
+++ src/main/java/com/algaworks/algalog/api/exceptionhandler/Problema.java
@@ -1,9 +1,11 @@
 package com.algaworks.algalog.api.exceptionhandler;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
@@ -11,4 +13,14 @@ public class Problema {
     private Integer status;
     private LocalDateTime dataHora;
     private String titulo;
+    private List<Campo> campos;
+
+
+    @Getter
+    @AllArgsConstructor
+    public static class Campo{
+        private String nome;
+        private String mensagem;
+
+    }
 }
