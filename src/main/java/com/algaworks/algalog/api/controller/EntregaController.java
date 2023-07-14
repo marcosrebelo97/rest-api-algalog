462549235b6371e4fb98f295904bcef841a23d94
marcosrebelo97 <mgr@icomp.ufam.edu.br>
2023-07-14 15:39:26 -0400

    implementação solicitação de entrega

---
+++ src/main/java/com/algaworks/algalog/api/controller/EntregaController.java
@@ -0,0 +1,23 @@
+package com.algaworks.algalog.api.controller;
+
+import com.algaworks.algalog.domain.service.EntregaService;
+import com.algaworks.algalog.domain.model.Entrega;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/entregas")
+public class EntregaController {
+
+    @Autowired
+    private EntregaService entregaService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Entrega solicitarEntrega(@RequestBody Entrega entrega){
+        return entregaService.solicitar(entrega);
+    }
+}
