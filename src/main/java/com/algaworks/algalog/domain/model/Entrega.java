6f16c28818877ff80ab1fdf8279e39831a21fefc
marcosrebelo97 <mgr@icomp.ufam.edu.br>
2023-07-14 15:53:55 -0400

    add anotação json/jackson - read_only

--- src/main/java/com/algaworks/algalog/domain/model/Entrega.java
+++ src/main/java/com/algaworks/algalog/domain/model/Entrega.java
@@ -1,5 +1,6 @@
 package com.algaworks.algalog.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -27,8 +28,13 @@ public class Entrega {
 
     private BigDecimal taxa;
 
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     @Enumerated(EnumType.STRING)
     private StatusEntrega status;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private LocalDateTime dataPedido;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private LocalDateTime dataFinalizacao;
 }
